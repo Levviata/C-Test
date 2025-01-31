dbf4526f6e57d5192745aab5580e92f1b066219c
Levviata <68709348+Levviata@users.noreply.github.com>
2025-01-31 23:09:39 +0100

    Bit of a cleanup

--- src/me/levviata/voxelgame/main.c
+++ src/me/levviata/voxelgame/main.c
@@ -1,4 +1,4 @@
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -8,9 +8,9 @@ static SDL_Vertex vert[vertLen];
 static SDL_Window *window = NULL; // the window where our rendering takes place in (and game also, its a window duh)
 static SDL_Renderer *renderer = NULL; // the variable where we will mash shit-in to print a beautiful or disfigured picture (WHEN we actually tell the GPU to render it)
 
-static const char appname[32] = "Voxel Game";
-static const char version[32] = "v0.0.0";
-static const char appIdentifier[32] = "me.levviata.voxelgame";
+static const char appname[] = "Voxel Game";
+static const char version[] = "v0.0.0";
+static const char appIdentifier[] = "me.levviata.voxelgame";
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -77,22 +77,6 @@ void render()
     SDL_RenderGeometry(renderer, NULL, vert, vertLen, NULL, 0);
 
     SDL_RenderPresent(renderer);
-
-    /*
-    SDL_FRect dst_rect;
-
-    SDL_RenderClear(renderer); // Clear the canvas/render screen
-
-    /* center this one.
-    dst_rect.x = ((float)(800 - texture_width)) / 2.0f;
-    dst_rect.y = ((float)(600 - texture_height)) / 2.0f;
-    dst_rect.w = (float)texture_width;
-    dst_rect.h = (float)texture_height;
-    SDL_RenderTexture(renderer, texture, NULL, &dst_rect);
-
-    //SDL_RenderTexture(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    */
 }
 
 /* This function runs once per frame, and is the heart of the program. */
@@ -108,11 +92,12 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    /* Seems like SDL destroys our render and window on app quit automatically? Either way leave this as is just in case. */
+    /* Seems like SDL destroys our render and window on app quit automatically?
     SDL_DestroyRenderer(renderer); // Destroy render 
-    SDL_DestroyWindow(window); // Destroy window
+    SDL_DestroyWindow(window); // Destroy window  */
 
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
-    SDL_Quit();
+
+    //SDL_free(appstate);
 }
 
